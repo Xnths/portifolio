f37b7baf9d1397a93518b3a0657ad579c42e92f7
Jonathas Castilho <jonathascastilhoes@gmail.com>
2022-02-17 17:20:42 -0300

    Esbolço da página Principal

---
+++ src/Principal.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Principal extends Component {
+    render() {
+        return (
+            <div>
+                <h2 className="h2__titulo">Sites para o seu negócio</h2>
+                <p className="p__descricao">Criação de sites profissionais por <storng className="destaque__desenvolvedor">Xnths</storng>, calouro na Universidade de São Paulo.</p>
+            </div>
+        )
+    }
+}
+
+export default Principal;
