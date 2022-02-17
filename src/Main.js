f55f6dc1064fa6467e85bf95ad94ec86302941c8
Jonathas Castilho <jonathascastilhoes@gmail.com>
2022-02-17 17:14:15 -0300

    Criando navegação

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+    color: blue;
+`
 
 class Main extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+                <h1 className="h1__titulo">Criação de Sites</h1>
+                <Nav>
+                    <ul>
+                        <li><a href="/">Principal</a></li>
+                        <li><a href="/orcamento">Orçamento</a></li>
+                    </ul>
+                </Nav>
+                <div className="conteudo"></div>
             </div>
         )
     }
