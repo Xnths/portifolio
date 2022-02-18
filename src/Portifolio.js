098e9579ede8247fc6d3da28cd2cc8cc8e76e43c
Jonathas Castilho <jonathascastilhoes@gmail.com>
2022-02-18 17:26:46 -0300

    Esbolço da página Portifolio

---
+++ src/Portifolio.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Portifolio extends Component {
+    render() {
+        return (
+            <div>
+                <h2 className="h2__titulo">Portifolio</h2>
+                <p className="p__descricao">Projetos em destaque</p>
+            </div>
+        )
+    }
+}
+
+export default Portifolio;
